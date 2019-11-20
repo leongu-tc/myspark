6ea7b0af55d264a0d4deb2f1b7241f8673d3624a
leongu-tc <gulele2003@qq.com>
2019-11-20 13:24:27 +0800

    #29 add business 积分系统. test ok

--- src/main/scala/leongu/myspark/_business/points/external/KafkaSink.scala
+++ src/main/scala/leongu/myspark/_business/points/external/KafkaSink.scala
@@ -1,7 +1,9 @@
 package leongu.myspark._business.points.external
 
+import java.util.Properties
 import java.util.concurrent.Future
 
+import leongu.myspark._business.points.util.PointCons
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
 
 import scala.collection.mutable
@@ -21,10 +23,10 @@ object KafkaSink {
 
   import scala.collection.JavaConversions._
 
-  def apply[K, V](config: mutable.Map[String, Object]): KafkaSink[K, V] = {
+  def apply[K,V](config: Map[String,Object]):KafkaSink[K,V]= {
     val createProducerFunc = () => {
-      val producer = new KafkaProducer[K, V](config)
-      sys.addShutdownHook {
+      val producer = new KafkaProducer[K,V](config)
+      sys.addShutdownHook{
         producer.close()
       }
       producer
@@ -32,4 +34,5 @@ object KafkaSink {
 
     new KafkaSink(createProducerFunc)
   }
+  def apply[K, V](config: java.util.Properties): KafkaSink[K,V] = apply(config.toMap)
 }
--- src/main/scala/leongu/myspark/_business/points/util/PointCons.scala
+++ src/main/scala/leongu/myspark/_business/points/util/PointCons.scala
@@ -38,14 +38,98 @@ trait PointCons {
 
   /** sql */
   lazy val logDate = conf.getOrElse(LOG_DATE, new SimpleDateFormat("yyyyMMdd").format(yesterday.getTime)).toString
-  lazy val individual_cust = "C" // cust_id,cust_telno
+  val individual_cust = "C" // cust_id,cust_telno
   // individual custom
-  lazy val CUSTBASEINFO_SQL = s"SELECT custid as cust_id,mobileno as cust_telno FROM centrd.custbaseinfo WHERE busi_date='$logDate' AND singleflag = 0"
+  lazy val CUSTBASEINFO_SQL = s"SELECT string(custid) as cust_id,mobileno as cust_telno FROM centrd.custbaseinfo WHERE singleflag = 0"
   lazy val busi_sqls = List(
     s""" SELECT CU.cust_id, CU.cust_telno, '010109' as busi_no, '$logDate' as busi_date, C2.orgid
-       | FROM (SELECT C.cust_id,C.cust_telno FROM C LEFT JOIN kbssacct.user_basic_info as U ON C.cust_id == U.user_code WHERE U.open_source = 1) as CU
-       | LEFT JOIN centrd.customer as C2 ON CU.cust_id == C2.custid WHERE C2.busi_date='$logDate'
+       | FROM (SELECT C.custid as cust_id,C.mobileno as cust_telno
+       |        FROM centrd.custbaseinfo C INNER JOIN kbssacct.user_basic_info as U
+       |        ON C.custid == U.user_code
+       |        WHERE U.open_source = 1 AND C.opendate = '$logDate' AND C.singleflag = 0) as CU
+       | LEFT JOIN centrd.customer as C2 ON CU.cust_id == C2.custid
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010201' as busi_no, '$logDate' as busi_date
+       | FROM centrd.cgemsecuinfo U INNER JOIN C
+       | ON C.cust_id = U.custid
+       | WHERE dealresult = '00' AND U.operdate='$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010301' as busi_no, U.prof_sign_date as busi_date
+       | FROM C INNER JOIN zh20.cust_invest_pro U
+       | ON C.cust_id = U.cust_code
+       | WHERE U.prof_investor_type='1' AND U.prof_sign_date='$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010401' as busi_no, '$logDate' as busi_date
+       | FROM centrd.secuid U INNER JOIN C
+       | ON C.cust_id = U.custid
+       | WHERE U.securight in ('0w') AND U.status = 0 and U.opendate='$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010501' as busi_no, '$logDate' as busi_date
+       | FROM C INNER JOIN ygt.opp_busi_data U
+       | ON C.cust_id = U.cust_code
+       | WHERE U.busi_code='D0008' AND U.proc_status='13'
+       | AND replace(to_date(U.oper_time),'-','')= '$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010601' as busi_no, '$logDate' as busi_date
+       | FROM C INNER JOIN ygt.opp_busi_data U
+       | ON C.cust_id = U.cust_code
+       | WHERE U.busi_code='D0009' AND U.proc_status='13'
+       | AND replace(to_date(U.oper_time),'-','')= '$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010701' as busi_no, '$logDate' as busi_date
+       | FROM C INNER JOIN kbssacct.cust_agreement U
+       | ON C.cust_id = U.cust_code
+       | WHERE U.cust_agmt_type = '11' and U.eft_date='$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010801' as busi_no, '$logDate' as busi_date
+       | FROM C INNER JOIN zh20.private_fund_invest U
+       | ON C.cust_id = U.cust_code
+       | WHERE U.fund_invest_type = '1' AND U.apply_date='$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'010901' as busi_no, '$logDate' as busi_date
+       |  FROM kbssacct.cust_agreement U INNER JOIN C
+       |  ON C.cust_id = U.cust_code
+       |  WHERE U.cust_agmt_type = '26' AND U.eft_date='$logDate'
+     """.stripMargin,
+//    s"""SELECT C.cust_id,C.cust_telno,'011001' as busi_no, '$logDate' as busi_date, U.market
+//       |  FROM centrd.secuid U INNER JOIN C
+//       |  ON C.cust_id = U.custid
+//       |  WHERE U.securight IN ('0s') AND U.status = 0 AND U.opendate='$logDate'
+//     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'011001' as busi_no, '$logDate' as busi_date,
+       |  element_at(map('5', '沪港通', 'S', '深港通', '5,S', 'both', 'S,5', 'both'), U2.markets) as market
+       |  FROM (SELECT U.custid, concat_ws(',' ,collect_set(U.market)) AS markets
+       |        FROM centrd.secuid U
+       |        where U.securight in ('0s') AND U.status = 0 AND U.opendate='$logDate' GROUP BY custid)
+       |  AS U2 INNER JOIN C
+       |  ON C.cust_id = U2.custid
+    """.stripMargin,
+  s"""SELECT C.cust_id,C.cust_telno,'011101' as busi_no, '$logDate' as busi_date
+       |  FROM martrd.cdtapplication U INNER JOIN C
+       |  ON C.cust_id = U.custid
+       |  WHERE effectivedate='$logDate' AND U.status='5'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'011201' as busi_no, '$logDate' as busi_date
+       |  FROM centrd.secuid U INNER JOIN C
+       |  ON C.cust_id = U.custid
+       |  WHERE U.securight in ('05','06') AND U.status = 0 AND U.opendate='$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'011301' as busi_no, '$logDate' as busi_date
+       |  FROM jz61.opt_trdacct U INNER JOIN C ON C.cust_id = U.cust_code
+       |  WHERE U.effect_date='$logDate'
+     """.stripMargin,
+    s"""SELECT C.cust_id,C.cust_telno,'011401' AS busi_no,'$logDate' AS busi_date, ordersno, fundcode
+       |  FROM C INNER JOIN
+       |  (SELECT custid AS cust_id, ordersno, ofcode AS fundcode
+       |     FROM centrd.ofmatch WHERE trdid='240059' AND matchdate='$logDate'
+       |  UNION
+       |  SELECT V.cust_code AS cust_id, V.app_sno AS ordersno, W.inst_id AS fundcode
+       |     FROM otc41.otc_auto_invest_agr V LEFT JOIN otc41.otc_inst_base_info W
+       |     ON V.inst_sno = W.inst_sno
+       |     WHERE V.agr_stat='1' AND V.app_date='$logDate'
+       |  ) U ON C.cust_id = U.cust_id
      """.stripMargin
+
   )
 
 }
