88dc8c0fcfec976ef642144bb9832e964fe16d3d
leongu-tc <gulele2003@qq.com>
2020-04-02 15:06:26 +0800

    #43 fix ColumnApi test spark Datatype

--- src/main/scala/leongu/myspark/session/api/ColumnApi.scala
+++ src/main/scala/leongu/myspark/session/api/ColumnApi.scala
@@ -1,5 +1,20 @@
 package leongu.myspark.session.api
 
+import org.apache.spark.sql.types.{DataType, DecimalType}
+
 object ColumnApi {
 
+  case class Record(key: Int, value: String)
+
+  def main(args: Array[String]) {
+
+    val dType: DataType = DecimalType(24, 6)
+
+    dType match {
+      case DecimalType() => println("111111")
+      case _ => println("2222222")
+    }
+
+    println("done!")
+  }
 }
